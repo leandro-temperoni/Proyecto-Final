39d03606344e6e47984a3dbf191dd23b19afca13
ProyectosX <leandrotemperoni@gmail.com>
2014-05-12 18:13:01 -0300

    Posibles clases a utilizar

---
+++ app/src/main/java/com/caece/proyectofinal/Utils/Device.java
@@ -0,0 +1,136 @@
+package com.caece.proyectofinal.Utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Created by lea on 20/06/13.
+ */
+public class Device {
+
+    public static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+    public static String getPhoneNumber(Context context){
+
+        TelephonyManager tMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String mPhoneNumber = tMgr.getLine1Number();
+        return mPhoneNumber;
+
+    }
+
+    private static String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
+        }
+    }
+
+    public static float readCPUUsage() {
+        try {
+            RandomAccessFile reader = null;
+            try {
+                reader = new RandomAccessFile("/proc/stat", "r");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            String load = reader.readLine();
+
+            String[] toks = load.split(" ");
+
+            long idle1 = Long.parseLong(toks[5]);
+            long cpu1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[4])
+                    + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+            try {
+                Thread.sleep(360);
+            } catch (Exception e) {}
+
+            reader.seek(0);
+            load = reader.readLine();
+            reader.close();
+
+            toks = load.split(" ");
+
+            long idle2 = Long.parseLong(toks[5]);
+            long cpu2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[4])
+                    + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+            return (float)(cpu2 - cpu1) / ((cpu2 + idle2) - (cpu1 + idle1));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    public static long getCurrentRAM(Context context){
+
+        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        activityManager.getMemoryInfo(mi);
+        long availableMegs = mi.availMem / 1048576L;
+
+        return availableMegs;
+
+    }
+
+    public static long getTotalMemory() {
+        {
+            String str1 = "/proc/meminfo";
+            String str2;
+            String[] arrayOfString;
+            long initial_memory = 0;
+            try {
+                FileReader localFileReader = new FileReader(str1);
+                BufferedReader localBufferedReader = new BufferedReader(    localFileReader, 8192);
+                str2 = localBufferedReader.readLine();//meminfo
+                arrayOfString = str2.split("\\s+");
+                for (String num : arrayOfString) {
+                    //Log.i(str2, num + "\t");
+                }
+                //total Memory
+                initial_memory = Integer.valueOf(arrayOfString[1]).intValue() * 1024;
+                localBufferedReader.close();
+                return initial_memory / 1048576L ;
+            }
+            catch (IOException e)
+            {
+                return -1;
+            }
+        }
+
+    }
+
+    public static void liberarMemoria(Context context){
+
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningAppProcessInfo pid : am.getRunningAppProcesses()) {
+            am.killBackgroundProcesses(pid.processName);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/caece/proyectofinal/Utils/OSOperations.java
@@ -0,0 +1,73 @@
+package com.caece.proyectofinal.Utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lea on 19/06/13.
+ */
+public class OSOperations {
+
+    public static final int USER_APPS = 0;
+    public static final int SYSTEM_APPS = 1;
+
+    public static void desinstalarPaquete(Context context, String packageString){
+
+        Uri packageURI = Uri.parse("package:" + packageString);
+        Intent intent = new Intent(Intent.ACTION_DELETE, packageURI);
+        context.startActivity(intent);
+
+    }
+
+    public static ArrayList<ApplicationInfo> getInstalledApps(Context context, int filter){
+
+        final PackageManager pm = context.getPackageManager();
+
+        ArrayList<ApplicationInfo> packages = (ArrayList<ApplicationInfo>) pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        ArrayList<ApplicationInfo> apps =  new ArrayList<ApplicationInfo>();
+
+        for(ApplicationInfo applicationInfo : packages){
+
+            if((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == filter){
+
+                apps.add(applicationInfo);
+
+            }
+
+        }
+
+        return apps;
+
+    }
+
+    public static ArrayList<ActivityManager.RunningAppProcessInfo> getRunningApps(Context context) {
+
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        return (ArrayList<ActivityManager.RunningAppProcessInfo>) manager.getRunningAppProcesses();
+
+    }
+
+    public static ArrayList<ActivityManager.RunningServiceInfo> getRunningServices(Context context) {
+
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        return (ArrayList<ActivityManager.RunningServiceInfo>) manager.getRunningServices(100);
+
+    }
+
+    public static void abrirDetallesApp(Context context, String packageString){
+
+        Uri packageURI = Uri.parse("package:" + packageString);
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+        context.startActivity(intent);
+
+    }
+
+}
